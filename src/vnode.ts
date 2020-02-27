a1b9cd29faccea1f2a5c8f1a422e5aa91f070e1b
jwdzzhz777 <jwdzzhz777@gmail.com>
2020-02-27 20:20:48 +0800

    update: 完善patch 方法

--- src/vnode.ts
+++ src/vnode.ts
@@ -1,5 +1,6 @@
 import { elmOption } from './compiler';
 import { miniVue1 } from './miniVue';
+import { isRef } from './reactivity';
 
 export enum NodeType {
     Element = 1,
@@ -34,7 +35,7 @@ export class VNode {
     attrs: elmOption = {};
     event: EventOption = {};
     children: VNode[] = [];
-    el: Element | undefined;
+    el: Element | Text | undefined;
     nodeValue?: string;
     type: NodeType;
 
@@ -48,14 +49,22 @@ export class VNode {
     }
 }
 
+export const getValue = function(target: any) {
+    return isRef(target)? target.value : target;
+}
+
 const vnodeToElm = function(vnode: VNode) {
-    if (vnode.type ===  NodeType.Text) return document.createTextNode(vnode.nodeValue || '');
+    if (vnode.type ===  NodeType.Text) {
+        let el = document.createTextNode(getValue(vnode.nodeValue) || '');
+        vnode.el = el;
+        return el;
+    };
     if (!vnode.tagName) return;
 
     let el = document.createElement(vnode.tagName);
 
     for (let key in vnode.attrs) {
-        el.setAttribute(key, vnode.attrs[key]);
+        el.setAttribute(key, getValue(vnode.attrs[key]));
     }
     
     for (let key in vnode.event) {
@@ -72,11 +81,47 @@ const vnodeToElm = function(vnode: VNode) {
 }
 
 export const patch = function(oldVNode: VNode | undefined, vnode: VNode, instance: miniVue1) {
-    
     if (!oldVNode) {
         let el = vnodeToElm(vnode);
-        instance.vnode = vnode;
-        console.log(el);
         if (el && instance.el) instance.el.parentNode!.replaceChild(el, instance.el);
+        return;
+    }
+
+    if (!isSameVNode(oldVNode, vnode)) {
+        let el = vnodeToElm(vnode);
+        if (el && oldVNode.el) oldVNode.el.parentNode!.replaceChild(el, oldVNode.el);
+    } else {
+        if (vnode.type === NodeType.Text && oldVNode.nodeValue !== vnode.nodeValue) {
+            console.log(123, oldVNode);
+            vnode.nodeValue && (oldVNode.el!.nodeValue = vnode.nodeValue);
+        } else {
+            updateAttrs(oldVNode, vnode);
+
+            vnode.children.forEach((child: VNode, index: number) => void patch(oldVNode.children[index], child, instance));
+        }
+
+        vnode.el = oldVNode.el;
+    }
+}
+
+const isSameVNode = function(oldVNode: VNode, vnode: VNode) {
+    return oldVNode.type === vnode.type && oldVNode.tagName === oldVNode.tagName;
+}
+
+const updateAttrs = function(oldVNode: VNode, vnode: VNode) {
+    if (!(oldVNode.el instanceof Element)) return;
+    let { attrs = {} } = vnode;
+    let { attrs: oldAttrs = {} } = oldVNode;
+    // 设置新的和修改的属性
+    for (let key in attrs) {
+        if (!(key in oldAttrs) || oldAttrs[key] !== attrs[key]) {
+            oldVNode.el!.setAttribute(key, getValue(attrs[key]));
+        }
+    }
+    // 删除没有的属性
+    for (let key in oldAttrs) {
+        if (!(key in attrs)) {
+            oldVNode.el!.removeAttribute(key);
+        }
     }
 }

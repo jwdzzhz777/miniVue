04f195860ee3561c16a478b3474152fdd49283f1
jwdzzhz777 <jwdzzhz777@gmail.com>
2020-03-02 14:28:51 +0800

    update:重写 createApp

--- src/miniVue.ts
+++ src/miniVue.ts
@@ -6,56 +6,85 @@ interface miniVueOpitons {
     setup?: (fn: createElement) => any;
 }
 
-export class miniVue1 {
+interface App {
+    $option: miniVueOpitons;
+    component: ComponentInstance;
+    mount: (selector: string) => void
+}
+
+export interface ComponentInstance {
     $option: miniVueOpitons;
     vnode?: VNode;
     render?: Function;
     el?: Element;
-    _c = createElement;
-    _v = getValue;
+    _c: createElement;
+    _v: (target: any) => any;
     setupResult?: any;
-    proxy: object = {};
+    proxy: object;
     update?: ReactiveEffect;
-    constructor(option: miniVueOpitons) {
-        this.$option=option;
-    }
-    mount(selector: string) {
-        let vm = this;
-        let el = document.querySelector(selector);
-        if (!el) return;
-        vm.el = el;
-        let render = compile(el);
-        vm.render = render;
-        console.log(vm.$option);
-        if (vm.$option.setup) {
-            let setupResult = vm.$option.setup(createElement);
-            vm.setupResult = setupResult;
-            vm.proxy = new Proxy(vm, {
-                get: (target, key) => {
-                    if (key in setupResult) return setupResult[key];
-                    else return Reflect.get(target, key);
-                },
-                set: (target, key, value) => {
-                    if (key in setupResult) setupResult[key] = value;
-                    return true;
-                },
-                // 一定要有 has 不然 with 语句拿不到
-                has(target, key) {
-                    return key in setupResult || Reflect.has(target, key);
-                }
+}
+
+export const createApp = function(options: miniVueOpitons) {
+    let instance = createInstance(options);
+    let app: App = {
+        $option: options,
+        component: instance,
+        mount(selector) {
+            let el = document.querySelector(selector);
+            if (!el) return;
+
+            let instance = this.component;
+            
+
+            instance.el = el;
+            instance.render = compile(el);
+            
+            processSetup(instance);
+
+
+            instance.update = effect(function() {
+                let vnode = instance.render?.call(instance.proxy);
+                let oldVNode = instance.vnode;
+                instance.vnode = vnode;
+    
+                patch(oldVNode, vnode, instance);
             });
+
+            return app;
         }
+    };
 
-        vm.update = effect(function() {
-            let vnode = render.call(vm.proxy);
-            console.log('vnode', vnode);
-            let oldVNode = vm.vnode;
-            vm.vnode = vnode;
+    return app;
+};
 
-            patch(oldVNode, vnode, vm);
+const createInstance = function(options: miniVueOpitons): ComponentInstance {
+    let instance: ComponentInstance = {
+        $option: options,
+        _c: createElement,
+        _v: getValue,
+        proxy: {}
+    };
+
+    return instance;
+}
+
+const processSetup = function(instance: ComponentInstance) {
+    let { setup } = instance.$option;
+    if (setup) {
+        let setupResult = instance.setupResult = setup.call(instance, createElement);
+        instance.proxy = new Proxy(instance, {
+            get: (target: ComponentInstance, key: string) => {
+                if (key in setupResult) return setupResult[key];
+                else return Reflect.get(target, key);
+            },
+            set: (target: ComponentInstance, key: string, value: any) => {
+                if (key in setupResult) setupResult[key] = value;
+                return true;
+            },
+            // 一定要有 has 不然 with 语句拿不到
+            has(target: ComponentInstance, key: string) {
+                return key in setupResult || Reflect.has(target, key);
+            }
         });
     }
 }
-
-
-export default miniVue1;
